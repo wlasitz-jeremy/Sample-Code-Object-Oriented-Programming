f5c578c17925e218a8450111ee073da1adcd0bab
wlasitz-jeremy <wlasitz@gmail.com>
2026-01-28 12:40:51 -0700

    initial commit

--- 3-3.py
+++ 3-3.py
@@ -1,7 +1,6 @@
 yr = int(input('Enter year: '))
 if yr%4==0:
     if yr%100:
-        print(f'{yr} is a leap year')
     else:
         if yr%400==0:
             print(f'{yr} is a leap year')
@@ -9,5 +8,3 @@ if yr%4==0:
             print(f'{yr} is not a leap year')
 else:
     print(f'{yr} is not a leap year')
-
-
