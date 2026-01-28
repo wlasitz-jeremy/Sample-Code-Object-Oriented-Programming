c98c875cb4b8f0a58de0e6075421e8a5f3aafe6e
wlasitz-jeremy <wlasitz@gmail.com>
2026-01-28 12:17:04 -0700

    initial commit

--- 3-2.py
+++ 3-2.py
@@ -1,25 +1,25 @@
 grd = int(input('Enter grade point'))
 if grd>90 and grd<=100:
-    print('A+')
+    print(f'A+')
 elif grd>85 and grd<=89:
-    print('A')
+    print(f'A')
 elif grd>80 and grd<=84:
-    print('A-')
+    print(f'A-')
 elif grd>77 and grd<=79:
-    print('B+')
+    print(f'B+')
 elif grd>73 and grd<=76:
-    print('B')
+    print(f'B')
 elif 70<grd<72:
-    print('B-')
+    print(f'B-')
 elif grd>67 and grd<=69:
-    print('C+')
+    print(f'C+')
 elif grd>63 and grd<=66:
-    print('C')
+    print(f'C')
 elif grd>60 and grd<=62:
-    print('C-')
+    print(f'C-')
 elif grd>55 and grd<=59:
-    print('D+')
+    print(f'D+')
 elif grd>50 and grd<=54:
-    print('D')
+    print(f'D')
 else:
-    print('F')
+    print(f'F')
