7a9c1d3a473be9d010d48af162f43cb586a68031
wlasitz-jeremy <wlasitz@gmail.com>
2026-01-28 11:14:35 -0700

    initial commit

--- Test2.py
+++ Test2.py
@@ -1,7 +1,7 @@
 r = float(input('Enter radius'))
 h = float(input('Enter height'))
 pi=3.14
-volume=pi*(r**2)*h
-area=(2*pi)*h*r + pi*(r**2)
-print("Area="+str(area)+"\nVolume="+str(volume))
+volume=str(pi*(r**2)*h)
+area=str((2*pi)*h*r + pi*(r**2))
+print("Area="+area+"\nVolume="+volume)
 
