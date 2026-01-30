c1a93996902da5e7f3dd4560b8ebec5023291d68
wlasitz-jeremy <wlasitz@gmail.com>
2026-01-30 13:38:46 -0700

    initial commit

--- 4-2.py
+++ 4-2.py
@@ -7,3 +7,8 @@ grd = float(input('Enter number of grades: '))
 
 
 
+Write a program which:
+Asks the user to enter number of grades.
+Gets the grades from the user.
+Calculates the average of the grades
+Finds the maximum grade
