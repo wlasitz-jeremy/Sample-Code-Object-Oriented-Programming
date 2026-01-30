de4b4c7bdab3f8dfe9f51d6666f2da4cb72db2a5
wlasitz-jeremy <wlasitz@gmail.com>
2026-01-30 12:52:19 -0700

    initial commit

---
+++ 4-1.py
@@ -0,0 +1,20 @@
+course = 'Python'
+print(f'{course:>20}')
+print(f'{course:<20}')
+print(f'{course:20}')
+print(f'{"Centered":^20}\n')
+
+cadtd= 1
+usdtd= 0.74357
+cadytd= 1
+usdytd= 0.73658
+changeusd= (usdtd-usdytd)
+changecad= (cadtd-cadytd)
+print(f'{"Date":^10}{"Rate":^20}')
+print(f'Cad Yesterday{cadytd:^17.4f}')
+print(f'Cad Today{cadtd:^24.4f}')
+print(f'Cad Change{changecad:^22.4f}')
+print(f'Usd Yesterday{usdytd:^16.4f}')
+print(f'Usd Today{cadtd:^24.4f}')
+print(f'Usd Change{changeusd:^22.4f}')
+
