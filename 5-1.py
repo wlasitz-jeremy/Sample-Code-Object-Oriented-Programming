69604d8afec6ceb2ea589e6e98de31063c739df6
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-04 13:15:29 -0700

    initial commit

--- 5-1.py
+++ 5-1.py
@@ -1,9 +1,9 @@
 kpm=1.61
 print(f'{"MPH"}',f'{"KPH":>10}')
-print('==========',"==========")
+print(format('==========',"10s"), format("==========", "10s"))
 for mph in range(10,80,10):
     kph=mph*kpm
-    print(f'{mph:10d}{kph:>10.0f}')
+    print(format(mph,"10d"),format(kph,"10.0f"))
 
 
 
