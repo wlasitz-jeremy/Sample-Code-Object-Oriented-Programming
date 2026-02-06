ca9cd9d1368e6496e6c3da90c16f5f24e0ef5890
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-06 12:53:21 -0700

    initial commit

--- 6-1.py
+++ 6-1.py
@@ -1,12 +1,12 @@
+RC='Rock Climbing'
+print(RC[5:10])
+for height in range(0,len(RC)):
+    print(RC[height-8])
 
-
-
-
-
-
-
-
-
+height=0
+while height<len(RC):
+    print(RC[height])
+    height+=2
 
 
 
