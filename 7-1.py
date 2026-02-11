36b0c80d3a1dadae40f0c8348a1eac2eaaa468f5
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-11 12:57:34 -0700

    initial commit

--- 7-1.py
+++ 7-1.py
@@ -1,8 +1,15 @@
-names=('Joe', 'Ed', 'Fred')
-num=len(names)
-print(num)
-first=min(names)
-print(first)
-print(max(names))
-del names
-print(names)
+# names=('Joe', 'Ed', 'Fred')
+# num=len(names)
+# print(num)
+# first=min(names)
+# print(first)
+# print(max(names))
+# del names
+
+
+days=('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')
+print(days)
+weekdays=(f'Weekdays:{days[1:5]}')
+print(weekdays)
+weekends=(f'Weekends:{days[0]} {days[6]}')
+print(weekends)
